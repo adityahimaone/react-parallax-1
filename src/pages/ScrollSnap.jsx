a587c4285cb8aa8bb4ce5ee280cd569e9d1b6760
Aditya Himawan <30428194+adityahimaone@users.noreply.github.com>
2022-06-09 22:07:01 +0700

    test

--- src/pages/ScrollSnap.jsx
+++ src/pages/ScrollSnap.jsx
@@ -1,23 +1,23 @@
-import React from "react";
+// import React from "react";
 
-export default function ScrollSnap() {
-  return (
-    <div>
-      <div className="snap-proximity snap-y overflow-y-scroll h-screen">
-        <section className="one h-screen flex justify-center items-center bg-green-400 snap-start">
-          <h1 className="text-5xl text-white">TEXT</h1>
-        </section>
-        <section className="two h-screen flex justify-center items-center bg-blue-400 snap-start">
-          <h1 className="text-5xl text-white">TEXT</h1>
-        </section>
-        <section className="three h-screen flex justify-center items-center bg-yellow-400 snap-start">
-          <h1 className="text-5xl text-white">TEXT</h1>
-        </section>
-        <section className="four h-screen flex justify-center items-center bg-red-400 snap-start">
-          <h1 className="text-5xl text-white">TEXT</h1>
-        </section>
-      </div>
-    </div>
-  );
-}
-``
+// export default function ScrollSnap() {
+//   return (
+//     <div>
+//       <div className="snap-proximity snap-y overflow-y-scroll h-screen">
+//         <section className="one h-screen flex justify-center items-center bg-green-400 snap-start">
+//           <h1 className="text-5xl text-white">TEXT</h1>
+//         </section>
+//         <section className="two h-screen flex justify-center items-center bg-blue-400 snap-start">
+//           <h1 className="text-5xl text-white">TEXT</h1>
+//         </section>
+//         <section className="three h-screen flex justify-center items-center bg-yellow-400 snap-start">
+//           <h1 className="text-5xl text-white">TEXT</h1>
+//         </section>
+//         <section className="four h-screen flex justify-center items-center bg-red-400 snap-start">
+//           <h1 className="text-5xl text-white">TEXT</h1>
+//         </section>
+//       </div>
+//     </div>
+//   );
+// }
+// ``
